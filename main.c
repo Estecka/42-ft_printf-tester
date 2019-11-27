657feec483f2d8d159d154b8829525713408c3fd
Adrien Baur <abaur@student.42.fr>
2019-11-27 16:57:38 +0100

    Added tests for each numerical tags

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 /*   By: abaur <abaur@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/20 17:30:43 by abaur             #+#    #+#             */
-/*   Updated: 2019/11/26 14:32:14 by abaur            ###   ########.fr       */
+/*   Updated: 2019/11/27 17:42:37 by abaur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <limits.h>
 
 int    vprintf(const char* format, va_list args); // Defined in stdio.
 int ft_vprintf(const char* format, va_list args); // Needs to be defined by you.
@@ -50,7 +51,8 @@ void TestPrintf()
 	printf("\n\n\tft_vprintf\n");
 
 	TestOnePrintf("Boop");
-
+	TestOnePrintf("Vraie fausse alerte %%", 0, 0);
+	TestOnePrintf("Fausse vraie alerte %wysiwyg", 0);
 
 	TestOnePrintf("String %s", "Soupe au choux");
 	TestOnePrintf("Zeroed string %010s", "Gnouh");
@@ -58,21 +60,14 @@ void TestPrintf()
 	TestOnePrintf("Left padded string `%-010s`", "Poux");
 
 	TestOnePrintf("Characters %c, %5c, %05c, %-5c, %#-05c", '%', '5', ' ', '-', '0');
+	TestOnePrintf("Decimal %d, %d, %d, %015d, %-015d", 0, INT_MIN, INT_MAX, INT_MIN, INT_MAX);
+	TestOnePrintf("Unsigned %u, %u, %015u, %-015u", 0, UINT_MAX, INT_MAX, UINT_MAX);
+	TestOnePrintf("Octa %o, %o, %010o, %-010o", 0, UINT_MAX, INT_MAX, UINT_MAX);
+	TestOnePrintf("Hexa %x, %x, %010x, %-010x", 0, UINT_MAX, INT_MAX, UINT_MAX);
+	TestOnePrintf("HEXA %X, %X, %010X, %-010X", 0, UINT_MAX, INT_MAX, UINT_MAX);
 
-	TestOnePrintf("Digit %d", 5);
-	TestOnePrintf("Hexa %x %x", 0, 32);
-	TestOnePrintf("Octa %o %o", 0, 32);
-	TestOnePrintf("Vraie fausse alerte %%", 0, 0);
-	TestOnePrintf("Fausse vraie alerte %wysiwyg", 0);
 	TestOnePrintf("Double Padding !! %3-010d", 0);
-	TestOnePrintf("Je beguaie %-0-0d", 5);
-
-	TestOnePrintf("Padding de base %14d", -42);
-	TestOnePrintf("Padding a la con %#+010X", 256);
-	TestOnePrintf("Padding mesquin % 2d", 0);
-	TestOnePrintf("Mesquinerie sans nom %##X", 5);
-
-	TestOnePrintf("I got a %s, Woho, ho", "hangover");
+	TestOnePrintf("Je beguaie %-0-0d %", 5);
 }
 
 int	main()
